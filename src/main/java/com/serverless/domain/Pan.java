e7393ff0cfe78b54e21f9dc61ab592e5b2ae9e28
Jordi Farré <jordifr@gmail.com>
2018-06-07 07:47:35 +0200

    implement tokenization

---
+++ src/main/java/com/serverless/domain/Pan.java
@@ -0,0 +1,65 @@
+package com.serverless.domain;
+
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.EncryptRequest;
+import com.amazonaws.services.kms.model.EncryptResult;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class Pan {
+
+    private String value;
+
+    private Pan(String value) {
+        this.value = value;
+    }
+
+    public static Pan from(String value) {
+        return new Pan(value);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Token encrypt(AWSKMS awskms, AmazonS3 amazonS3) {
+        String token = createTokenFrom(value);
+        ByteBuffer encryptedPan = encrypt(awskms);
+        save(amazonS3, token, encryptedPan);
+        return Token.from(token);
+    }
+
+    private void save(AmazonS3 amazonS3, String token, ByteBuffer encryptedPan) {
+        InputStream inputStream = new ByteArrayInputStream(encryptedPan.array());
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setSSEAlgorithm("AES256");
+        PutObjectRequest putObjectRequest = new PutObjectRequest(System.getenv("PCI_BUCKET"), token, inputStream, metadata);
+        amazonS3.putObject(putObjectRequest);
+    }
+
+    private ByteBuffer encrypt(AWSKMS awskms) {
+        EncryptRequest encryptRequest = new EncryptRequest();
+        encryptRequest.setKeyId(System.getenv("KMS_ALIAS_ARN"));
+        encryptRequest.setPlaintext(ByteBuffer.wrap(this.value.getBytes(UTF_8)));
+        Map<String, String> encryptionContext = new HashMap<>();
+        encryptionContext.put("UserName", System.getenv("ENCRYPTION_CONTEXT_USER_NAME"));
+        encryptRequest.setEncryptionContext(encryptionContext);
+        EncryptResult encryptResult = awskms.encrypt(encryptRequest);
+        return encryptResult.getCiphertextBlob();
+    }
+
+    private String createTokenFrom(String pan) {
+        return UUID.fromString(value).toString();
+    }
+}
--- src/main/java/com/serverless/infrastructure/Tokenizer.java
+++ src/main/java/com/serverless/infrastructure/Tokenizer.java
@@ -1,10 +1,14 @@
 package com.serverless.infrastructure;
 
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.serverless.domain.Pan;
+import com.serverless.domain.Token;
 import org.apache.log4j.Logger;
-
-import java.util.Collections;
 
 public class Tokenizer implements RequestHandler<ApiGatewayRequestDTO, ApiGatewayResponseDTO> {
 
@@ -12,11 +16,14 @@ public class Tokenizer implements RequestHandler<ApiGatewayRequestDTO, ApiGatewa
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
+	private AWSKMS awskms = AWSKMSClientBuilder.defaultClient();
+	private AmazonS3 amazonS3 = AmazonS3ClientBuilder.defaultClient();
+
 	@Override
 	public ApiGatewayResponseDTO handleRequest(ApiGatewayRequestDTO input, Context context) {
-		LOG.info("received: " + input.getBody());
 		TokenizerRequestDTO tokenizerRequestDTO = objectMapper.readValue(input.getBody(), TokenizerRequestDTO.class);
-		LOG.info("received: " + tokenizerRequestDTO.getPan());
-		return ApiGatewayResponseDTO.from(200, objectMapper.writeValueAsString(TokenizerResponseDTO.from("to implement")));
+		Token token = Pan.from(tokenizerRequestDTO.getPan())
+				.encrypt(awskms, amazonS3);
+		return ApiGatewayResponseDTO.from(200, objectMapper.writeValueAsString(TokenizerResponseDTO.from(token.getValue())));
 	}
 }
